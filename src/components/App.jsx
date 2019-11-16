b1a2b65f2df3383decfffc4ae46e0ef67408cc01
proforty7 <proforty7@gmail.com>
2019-11-16 14:40:46 +0530

    add mapbox with react-map-gl add geojson constant for now
    Signed-off-by: proforty7 <proforty7@gmail.com>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Source, Layer } from "react-map-gl";
+import { MAPBOX_ACCESS_TOKEN } from "../config";
 
 const App = () => {
-  return <div>app</div>;
+  const [state, setState] = useState({
+    geojson: {
+      type: "FeatureCollection",
+      features: []
+    }
+  });
+
+  const [viewPort, setViewPort] = useState({
+    zoom: 6.8,
+    bearing: 0,
+    pitch: 0,
+    dragPan: true,
+    width: 600,
+    height: 600,
+    latitude: 38.8951,
+    longitude: -77.0364
+  });
+
+  const geojson = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        geometry: { type: "Point", coordinates: [102.0, 0.5] },
+        properties: { prop0: "value0" }
+      },
+      {
+        type: "Feature",
+        geometry: {
+          type: "LineString",
+          coordinates: [
+            [102.0, 0.0],
+            [103.0, 1.0],
+            [104.0, 0.0],
+            [105.0, 1.0]
+          ]
+        },
+        properties: {
+          prop0: "value0",
+          prop1: 0.0
+        }
+      },
+      {
+        type: "Feature",
+        geometry: {
+          type: "Polygon",
+          coordinates: [
+            [
+              [100.0, 0.0],
+              [101.0, 0.0],
+              [101.0, 1.0],
+              [100.0, 1.0],
+              [100.0, 0.0]
+            ]
+          ]
+        },
+        properties: {
+          prop0: "value0",
+          prop1: { this: "that" }
+        }
+      }
+    ]
+  };
+  return (
+    <ReactMapGL
+      {...viewPort}
+      onViewportChange={setViewPort}
+      mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+    >
+      <Source id="my-data" type="geojson" data={geojson}>
+        <Layer
+          id="point"
+          type="circle"
+          paint={{
+            "circle-radius": 10,
+            "circle-color": "#007cbf"
+          }}
+        />
+      </Source>
+    </ReactMapGL>
+  );
 };
 
 export default App;
---
+++ src/config/index.js
@@ -0,0 +1,2 @@
+export const MAPBOX_ACCESS_TOKEN =
+  "pk.eyJ1IjoicHJvZm9ydHk3IiwiYSI6ImNrMnlmcW50NDA3MTIzbmt0dmEwZmJwYXoifQ.nTNYCB_Gku_pUBWF6Ko_HQ";
