0b3b419909eb0b2c0e32cbb33a317f51c55a030c
proforty7 <proforty7@gmail.com>
2019-11-21 01:16:43 +0530

    add handlechange in property panel
    Signed-off-by: proforty7 <proforty7@gmail.com>

--- src/components/elements/PropertyPanel.jsx
+++ src/components/elements/PropertyPanel.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { Card, Form, Input } from "semantic-ui-react";
 import { convertHex } from "../../utils";
+import { useDispatch, useSelector } from "react-redux";
+import { setGeoJSON } from "../../actions";
 
-const PropertyPanel = ({ properties, setProperties }) => {
+const PropertyPanel = () => {
+  const dispatch = useDispatch();
+  const geojson = useSelector(state => state.geojsonState.geojson);
+  const selectedFeatures = useSelector(
+    state => state.geojsonState.selectedFeatures
+  );
+
   const handleChange = (_, { name, value }) => {
     let opacity = 255;
     if (name === "color" || name === "fillColor") {
@@ -10,7 +18,15 @@ const PropertyPanel = ({ properties, setProperties }) => {
       value = convertHex(value, opacity);
     }
     if (name === "width") value = parseInt(value);
-    setProperties({ ...properties, [name]: value });
+    const newFeatures = geojson.features.map((feature, index) => {
+      const newFeature = feature;
+      if (selectedFeatures.indexOf(index) !== -1) {
+        newFeature.properties = { ...newFeature.properties, [name]: value };
+      }
+      return newFeature;
+    });
+    dispatch(setGeoJSON({ ...geojson, features: newFeatures }));
+    console.log(geojson);
   };
 
   return (
