69f41c6208c749387e655cd67cfd9662a7e17693
proforty7 <proforty7@gmail.com>
2019-11-21 01:24:47 +0530

    fix error
    Signed-off-by: proforty7 <proforty7@gmail.com>

--- src/components/LeftPane.jsx
+++ src/components/LeftPane.jsx
@@ -2,24 +2,12 @@ import React, { useState } from "react";
 import Dragger from "./elements/Dragger";
 import ReactJSONView from "react-json-view";
 import { Icon, Accordion, Button } from "semantic-ui-react";
-<<<<<<< HEAD
-import PropertyPanel from "./elements/PropertyPanel";
-
-const LeftPane = ({
-  geojson,
-  setgeojson,
-  setMode,
-  properties,
-  setProperties
-}) => {
-=======
 import { useSelector, useDispatch } from "react-redux";
 import { setGeoJSON } from "../actions";
 import PropertyPanel from "./elements/PropertyPanel";
 
 const LeftPane = ({ setMode }) => {
   const dispatch = useDispatch();
->>>>>>> origin/dev
   const [active, setActive] = useState(false);
   const geojson = useSelector(state => state.geojsonState.geojson);
 
@@ -37,11 +25,7 @@ const LeftPane = ({ setMode }) => {
           <Button onClick={() => setMode("edit")}>Edit</Button>
         </Button.Group>
       </div>
-<<<<<<< HEAD
-      <PropertyPanel properties={properties} setProperties={setProperties} />
-=======
       <PropertyPanel />
->>>>>>> origin/dev
       {geojson && (
         <Accordion fluid>
           <Accordion.Title active={active} onClick={() => setActive(!active)}>
